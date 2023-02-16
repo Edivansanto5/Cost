f83f157ef922d61b86e9faf74c8afa043dd13def
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-02-16 10:32:45 -0300

    importando os dois componentes da pagina

--- costs/src/routes/index.js
+++ costs/src/routes/index.js
@@ -1,3 +1,6 @@
 import {BrowserRouter, Route,Routes} from "react-router-dom"
 import React from "react"
 
+import Home from "../components/pages/Home"
+import Company from "../components/pages/Company"
+
