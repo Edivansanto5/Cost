a58c15816e0c0e1298e46363a201c99e282904dd
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-02-28 14:57:05 -0300

    importando icons da bliblioteca react-icons

--- projeto-cost/src/Components/Layout/Footer.js
+++ projeto-cost/src/Components/Layout/Footer.js
@@ -1,9 +1,24 @@
-function Footer(){
-    return(
-        <div>
-            
-            <footer>EdivanSantos2023</footer>
-        </div>
+import { FaFacebook, FaInstagram, FaLinkedin } from'react-icons/fa'
+import styles from '../CSS/Footer.module.css'
+
+
+function Footer() {
+    return (
+        <footer >
+            <ul className={styles.footer}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaLinkedin/>
+                </li>
+            </ul>
+
+
+        </footer>
     )
 }
 export default Footer;
