8b9b09e5cdd3fc5961ebfa07c7c6843bc534739a
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-02-28 10:40:41 -0300

    add o logo no componente navbar

--- projeto-cost/src/Components/Layout/Navbar.js
+++ projeto-cost/src/Components/Layout/Navbar.js
@@ -1,10 +1,13 @@
 import Container from "./Container";
 import { Link } from "react-router-dom";
+import style from '../CSS/Navbar.module.css'
+import logo from '../../../src/img/costs_logo.png'
 
 function Navbar(){
     return(
         <nav>
             <Container>
+                <Link to='/'> <img src={logo} alt="cost" /></Link>
                 <Link to='/'>Home</Link>
                 <Link to='/contato'>Contato</Link>
                 <Link to='/novoProjeto'>Novo Projeto</Link>
