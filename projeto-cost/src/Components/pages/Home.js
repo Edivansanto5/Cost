63c06bb2fda250d605631f29754e6701a43ffe37
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-03-01 12:14:55 -0300

    estilizando titulo,span,paragrafo e imagen

--- projeto-cost/src/Components/pages/Home.js
+++ projeto-cost/src/Components/pages/Home.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import img from '../../img/savings.svg'
+import styles from '../CSS/Home.module.css'
+
 function Home(){
     return(
-        <section>
+        <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Cost</span></h1>
             <p>Comece a gerenciar seus projetos agora mesmo</p>
             <Link>Novo Projeto</Link>
