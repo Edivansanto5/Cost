2fcc99233ff9d09a3b4d692fb1378893b34ee052
Edivansanto5 <edivansantos1478@gmail.com>
2023-03-02 16:23:03 -0300

    alterando nome da class

--- projeto-cost/src/Components/Form/Input.js
+++ projeto-cost/src/Components/Form/Input.js
@@ -2,7 +2,7 @@ import styles from '../CSS/Input.module.css'
 
 function Input({type,text,name,placeholder,handleOnchange,value}){
   return(
-    <div className={styles.input}> 
+    <div className={styles.form_control}> 
       <label htmlFor=""></label>
       <input type="text" name="" id="" />
     </div>
