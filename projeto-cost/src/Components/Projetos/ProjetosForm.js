3402aa69c83899f4a688006ef735c32ed572aa7c
Edivansanto5 <edivansantos1478@gmail.com>
2023-03-02 16:05:48 -0300

    criando estilos para o formulario de criaçao de projetos

--- projeto-cost/src/Components/Projetos/ProjetosForm.js
+++ projeto-cost/src/Components/Projetos/ProjetosForm.js
@@ -1,23 +1,24 @@
+import styles from '../CSS/ProjetosForm.module.css'
+
 function ProjetoForm(){
     return(
-        <div>
-            <form>
-                <div>
-                    <input type="text" placeholder="Informe o nome do Projeto" />
-                </div>
-                <div>
-                    <input type="number" placeholder="Informe o Orçamento Total" />
-                </div>
-                <div>
-                <select name="category_id">
-                    <option disabled selected >Selecione a Categoria</option>
-                </select>
-                </div>
-                <div>
-                   <input type='submit' value='Criar Projeto'/>
-                </div>   
-            </form>
-        </div>
+        
+        <form className={styles.form}>
+            <div>
+                <input type="text" placeholder="Informe o nome do Projeto" />
+            </div>
+            <div>
+                <input type="number" placeholder="Informe o Orçamento Total" />
+            </div>
+            <div>
+            <select name="category_id">
+                <option disabled selected >Selecione a Categoria</option>
+            </select>
+            </div>
+            <div>
+                <input type='submit' value='Criar Projeto'/>
+            </div>   
+        </form>
     )
 }
 
