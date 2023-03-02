8de753643dcebb28dd1ddd4d85e2513dcde22a9f
Edivansanto5 <edivansantos1478@gmail.com>
2023-03-02 14:45:31 -0300

    finalizando o estilo do componente NovoProjeto

--- projeto-cost/src/Components/pages/NovoProjeto.js
+++ projeto-cost/src/Components/pages/NovoProjeto.js
@@ -1,6 +1,8 @@
+import styles from'../CSS/NovoProjeto.module.css'
+
 function NovoProjeto(){
     return(
-        <div>
+        <div className={styles.novoprojeto_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu Projeto para depois adicionar os Serviços</p>
             <p>aqui a baixo vai vim um formulario</p>
