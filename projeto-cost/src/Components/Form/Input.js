96e7c1378d35f804fe31ab43059cb1118ae67e64
Edivansanto5 <edivansantos1478@gmail.com>
2023-03-02 16:14:06 -0300

    criando a estrutura do input

--- projeto-cost/src/Components/Form/Input.js
+++ projeto-cost/src/Components/Form/Input.js
@@ -1,6 +1,11 @@
+import styles from '../CSS/Input.module.css'
+
 function Input(){
   return(
-    <div></div>
+    <div className={styles.input}> 
+      <label htmlFor=""></label>
+      <input type="text" name="" id="" />
+    </div>
   )
 }
 export default Input;
